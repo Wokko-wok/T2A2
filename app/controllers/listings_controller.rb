ce9a9569253cfabe525172c805ab9de34d6dbfa8
Wokko-wok <philliplam9@gmail.com>
2022-07-09 11:20:08 +1000

    active storage not working

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -75,6 +75,6 @@ class ListingsController < ApplicationController
     end
     # Only allow a list of trusted parameters through.
     def listing_params
-      params.require(:listing).permit(:user_id, :species, :age, :price, :name, :description, :sold)
+      params.require(:listing).permit(:user_id, :species, :age, :price, :name, :description, :sold, :picture)
     end
 end
